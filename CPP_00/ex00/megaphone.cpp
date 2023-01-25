d634864f3f97fdcd2864c75bfb1bb50034d8db05
Merel van der Wal <Merelvanderwal94@gmail.com>
2023-01-25 15:02:01 +0100

    ex00 finished

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,11 +6,25 @@
 /*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/24 12:17:22 by mevan-de      #+#    #+#                 */
-/*   Updated: 2023/01/24 12:17:40 by mevan-de      ########   odam.nl         */
+/*   Updated: 2023/01/25 15:00:22 by mevan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+
+int main(int argc, char **argv)
 {
-	
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::string	str;
+	for (int i = 1; i < argc; i++){
+		str = argv[i];
+		for (int j = 0; j < str.length(); j++)
+			std::cout << (char) toupper(str[j]);
+	}
+	std::cout << std::endl;
+	return 0;
 }
